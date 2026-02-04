1d85a8ba45c046ee14669236297a9a612ecb38a0
Thor <87920551+marvel-fan-wd@users.noreply.github.com>
2026-02-04 18:11:01 +0800

    Delete Script/R directory

--- Script/R/BetaDiversity.R
+++
@@ -1,7 +0,0 @@
-library(vegan);library(ggplot2)
-
-df <- read.csv("E:/2025.8-2026.7/iScience_revision/Species_abundance.csv",row.names = 1,header = T)
-df <- t(df);otu.distance <- vegdist(df)
-otu.distance <- as.matrix(round(otu.distance,digits = 3))
-adonis2(otu.distance~Role, data=df, distance = "bray", permutations = 999, strata = df$SFID)
-
